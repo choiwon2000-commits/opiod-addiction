baebc7cff05ad5ec923d1a04288aa0bb79e3182f
wlaj <wlajansen16@gmail.com>
2025-09-13 16:07:47 +0200

    Fix image not loading

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL('https://cdn.sanity.io/images/00php9jv/**')],
+  },
 };
 
 export default nextConfig;
