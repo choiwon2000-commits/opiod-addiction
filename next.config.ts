650dea5a13c4a010d92505e282c0c5f0d9c32130
wlaj <wlajansen16@gmail.com>
2025-11-19 18:20:58 +0100

    Change url config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://cdn.sanity.io/images/00php9jv/**')],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/00php9jv/**',
+      },
+    ],
   },
 };
 
